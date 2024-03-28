8fcf05983f40deb43f5ddecda35fe0f17fec8210
babish9887 <babish9887@gmail.com>
2024-03-28 08:23:01 +0545

    updated mongoose

--- server.js
+++ server.js
@@ -7,18 +7,13 @@ process.on('uncaughtException', err => {
   process.exit(1);
 });
 
-const DB = 'mongodb://localhost:27017/natours';
-// const DB="mongodb+srv://babish9887:zSv5TDnxbCLfK56x@cluster0.mzljvwc.mongodb.net/natours?retryWrites=true&w=majority&appName=Cluster0"
+// const DB = 'mongodb://localhost:27017/natours';
+const DB="mongodb+srv://babish9887:zSv5TDnxbCLfK56x@cluster0.mzljvwc.mongodb.net/natours?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
   })
   .then(() => {
-    // console.log(conn.connections)
     console.log('DB connection Successfull');
   });
 
